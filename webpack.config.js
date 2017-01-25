fd40d01634f3bda901f6bc123933ec29598e23ee
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-25 19:39:45 +0800

    Added compact build procedure.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-var webpack = require( "webpack" );
+"use strict";
+
+const webpack = require( "webpack" );
+const ResolverPlugin = webpack.ResolverPlugin;
+const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = {
 	"entry": "./zelf.support.js",
 	"resolve": {
 		"modulesDirectories": [ "bower_components", "node_modules" ]
+	},
+	"module": {
+		"preLoaders": [
+			{
+				"test": /\.support\.js$/,
+				"loader": "source-map-loader"
+			}
+		]
 	},
 	"output": {
 		"library": "zelf",
@@ -11,8 +24,18 @@ module.exports = {
 		"filename": "zelf.deploy.js"
 	},
 	"plugins": [
-		new webpack.ResolverPlugin( new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
-		new webpack.ResolverPlugin( new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
-		new webpack.optimize.UglifyJsPlugin( { compress: { warnings: false } } )
-	]
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
+		new UglifyJsPlugin( {
+			"compress": {
+				"keep_fargs": true,
+				"keep_fnames": true,
+				"warnings": false
+			},
+			"comments": false,
+			"sourceMap": true,
+			"mangle": false
+		} )
+	],
+	"devtool": "#inline-source-map"
 };
