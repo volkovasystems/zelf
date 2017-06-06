a17bc857cbc60aaa7c5a49216239927b97deacd2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 12:51:58 +0800

    Updated test.

--- zelf-test.js
+++ zelf-test.js
@@ -1,5 +1,15 @@
-"use strict";
-
+const assert = require( "assert" );
 const zelf = require( "./zelf.js" );
 
-console.log( zelf( function test( ){ } ) );
+assert.equal( zelf( 123 ), 123, "should have value 123" );
+
+assert.deepEqual( zelf( { "name": "simple" } ), { "name": "simple" }, "should be equal" );
+
+assert.deepEqual( zelf( [ ] ), [ ], "should have empty array" );
+
+assert.deepEqual( zelf( [ { "name": "simple" } ] ), [ { "name": "simple" } ], "should be equal" );
+
+let test = function test( ){ };
+assert.equal( zelf( test ), test, "should be equal" );
+
+console.log( "ok" );
