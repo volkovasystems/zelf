898e9c72747f4716be1331a596f5be50fe8dd603
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-23 13:42:44 +0800

    Updated test.

--- test.module.js
+++ test.module.js
@@ -1,15 +1,70 @@
-const assert = require( "assert" );
+"use strict";
+
+/*;
+	@test-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-test-license
+
+	@test-configuration:
+		{
+			"package": "zelf",
+			"path": "zelf/test.module.js",
+			"file": "test.module.js",
+			"module": "test",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/zelf.git"
+		}
+	@end-test-configuration
+
+	@test-documentation:
+
+	@end-test-documentation
+
+	@include:
+		{
+			"assert": "should",
+			"zelf": "zelf"
+		}
+	@end-include
+*/
+
+const assert = require( "should" );
+
+//: @server:
 const zelf = require( "./zelf.js" );
+//: @end-server
 
-assert.equal( zelf( 123 ), 123, "should have value 123" );
+//: @client:
+const zelf = require( "./zelf.support.js" );
+//: @end-client
 
-assert.deepEqual( zelf( { "name": "simple" } ), { "name": "simple" }, "should be equal" );
-
-assert.deepEqual( zelf( [ ] ), [ ], "should have empty array" );
-
-assert.deepEqual( zelf( [ { "name": "simple" } ] ), [ { "name": "simple" } ], "should be equal" );
-
-let test = function test( ){ };
-assert.equal( zelf( test ), test, "should be equal" );
-
-console.log( "ok" );
+//: @bridge:
+const path = require( "path" );
+//: @end-bridge
